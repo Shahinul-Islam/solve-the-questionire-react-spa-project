35e5de58643d5d50c1d4029fa7fcb45572bc2c60
shahinul islam <shahinul37@outlook.com>
2022-10-12 06:54:30 +0600

    questionire sectiion completed

--- src/components/Qusetionire/Qusetionire.js
+++ src/components/Qusetionire/Qusetionire.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 
 const Qusetionire = () => {
   return (
     <div>
-      <h1> </h1>
+      <h1 className="text-2xl">
+        {" "}
+        Please visit{" "}
+        <Link to="/" className="text-blue-700">
+          Home
+        </Link>{" "}
+        and choose a category to see all the questions
+      </h1>
     </div>
   );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,30 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  BarChart,
-  Legend,
-  Bar,
-} from "recharts";
+import { XAxis, YAxis, Tooltip, BarChart, Bar } from "recharts";
 
 const Statistics = () => {
-  // const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
   const [qsnData, setData] = useState([]);
-  console.log(qsnData);
   useEffect(() => {
-    /* fetch("https://openapi.programming-hero.com/api/quiz")
-      .then((res) => res.json())
-      .then((data) => setData(data.data)); */
     axios
       .get("https://openapi.programming-hero.com/api/quiz")
       .then((data) => setData(data.data.data));
   }, []);
-  //   qsnData.map((data) => console.log(data));
   return (
     <BarChart width={550} height={450} data={qsnData}>
       <Bar dataKey="total" fill="#8884d8" />
